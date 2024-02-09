1adcdffa09c85aed84013cd1664b89a7881826be
Assylkhan Aldanazar <asilkhan-al@mail.ru>
2024-02-10 00:42:22 +0600

    Yerdaulet for you

---
+++ main.py
@@ -0,0 +1,18 @@
+from TikTokApi import TikTokApi
+import asyncio
+import os
+
+ms_token = os.environ.get(
+    "ms_token", "p_J0uwpbfdcWdiKpfT0KRNvggbjoEuo2EEwzIDb1ybR-WLk8erX-4DUgIeaZjFJmOv8aimeL53UmF06iDKP7gk3tUzSHC9vwgRWCAFOOXON7RvHgCBU4qIMTQ4CWp68m9Xq7agAVXeUahHEndw=="
+)  # set your own ms_token, needs to have done a search before for this to work
+
+
+async def search_users():
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False)
+        async for user in api.search.users("david teather", count=10):
+            print(user)
+
+
+if __name__ == "__main__":
+    asyncio.run(search_users())
---
+++ shmain.py
@@ -0,0 +1,20 @@
+from TikTokApi import TikTokApi
+import asyncio
+import os
+
+ms_token = os.environ.get(
+"ms_token", 
+"p_J0uwpbfdcWdiKpfT0KRNvggbjoEuo2EEwzIDb1ybR-WLk8erX-4DUgIeaZjFJmOv8aimeL53UmF06iDKP7gk3tUzSHC9vwgRWCAFOOXON7RvHgCBU4qIMTQ4CWp68m9Xq7agAVXeUahHEndw=="
+)  # set your own ms_token
+
+
+async def trending_videos():
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False)
+        async for video in api.trending.videos(count=30):
+            print(video)
+            print(video.as_dict)
+
+
+if __name__ == "__main__":
+    asyncio.run(trending_videos())
